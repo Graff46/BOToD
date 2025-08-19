4458215db5a6d28d9217c2fa6f2f07fb0ae298c7
Graff46 <macter46@yandex.ru>
2025-08-19 13:34:25 +0300

    .

--- site/index.js
+++ site/index.js
@@ -1,19 +1,1 @@
 var MyApp = App();
-
-(() => {
-	var idsLang = ['🇷🇺', '🇬🇧'];
-
-	changeTranslate.addEventListener(
-		'click', 
-		event => {
-			let lid = idsLang.indexOf(event.currentTarget.textContent);
-			event.currentTarget.textContent = idsLang[lid ^= 1];
-			localStorage.setItem('langId', lid);
-			return setLang(lid);
-		}
-	);
-
-	const langId = localStorage.getItem('langId') || 0;
-	changeTranslate.textContent = idsLang[langId];
-	setLang(langId);
-})();
