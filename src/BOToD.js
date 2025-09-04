ab9829fe49acb982b1fe3dc80b7ce942b9df8da2
Graff46 <macter46@yandex.ru>
2025-09-05 02:36:59 +0300

    Optimize

--- src/BOToD.js
+++ src/BOToD.js
@@ -1,14 +1,13 @@
 self.App = (() => {
 	var getEl = el => (el instanceof Element) ? el : document.querySelector(el);
-	var IS_PROXY = Symbol('isProxy');
-	var MASK = Symbol('mask');
-	var MAP = Symbol('map');
+	var _IS_PROXY = Symbol('isProxy');
+	var _MASK = Symbol('mask');
+	var _MAP = Symbol('map');
 
 	return (settingBits = 0) => {
 		var EVENT_TYPE = settingBits & 0b1 ? 'input' : 'change';
 		var BINDING_PROPERTY = settingBits & 0b10 ? 'textContent' : 'value';
 
-		var rootObj			= null;
 		var currentObjProp	= null;
 
 		var el2handlerBind	= new WeakMap();
@@ -31,7 +30,6 @@ self.App = (() => {
 			insert(code);
 
 			var nullCnt = 0;
-			if (code < 1) return console.error(code);
 			while(code !== 1) {
 				code >>= 1;
 				if (code % 2) {
@@ -53,7 +51,7 @@ self.App = (() => {
 
 			el2handlerBind.set(el, story);
 
-			var code = currentObjProp.obj[MASK];
+			var code = currentObjProp.obj[_MASK];
 
 			(tmp = bindUpd[code]) || (tmp = bindUpd[code] = Object.create(null));
 			(tmp[currentObjProp.prop] = (tmp[currentObjProp.prop] || new Set())).add(el);
@@ -68,7 +66,7 @@ self.App = (() => {
 			El2group.set(el, group);
 
 			skipProxyGetFlg = true;
-			const msk = currentObjProp.obj[currentObjProp.prop][MASK];
+			const msk = currentObjProp.obj[currentObjProp.prop][_MASK];
 			skipProxyGetFlg = false;
 
 			currentObjProp = null;
@@ -78,18 +76,8 @@ self.App = (() => {
 
 		var updRepeat = (el, group) => El2group.set(el, group);
 
-		var _unbind = (el, onlyBind) => {
-			if (el[IS_PROXY]) return _unbindObj(el);
-
-			const elm = getEl(el);
-
-			el2handlerBind.delete(elm);
-
+		var resetEl = elm => {
 			elm.value = null;
-
-			if (onlyBind) return;
-
-			el2handlerRept.delete(elm);
 
 			const group = El2group.get(elm);
 			if (group) {
@@ -101,18 +89,37 @@ self.App = (() => {
 			El2group.delete(elm);
 			(tmp = el2eventHandler.get(elm)) && (elm.removeEventListener(EVENT_TYPE, tmp));
 			el2eventHandler.delete(elm);
+		};
+
+		var _unbind = (el, onlyBind) => {
+			if (el[_IS_PROXY]) return _unbindObj(el);
+
+			const elm = getEl(el);
+
+			el2handlerBind.delete(elm);
+
+			elm.value = null;
+
+			if (onlyBind) return;
+
+			el2handlerRept.delete(elm);
+
+			return resetEl(elm);
 		}
 
-		var _unbindObj = (obj) => {
-			for (var code = obj[MASK]; code <= maxCode; code++) {
-				[bindReset, bindUpd, repeatStore].forEach(acc => {
-					if (tmp = acc[code] && Object.values(acc[code])) {
-						tmp.forEach(el => _unbind(el, true)); 
-						delete bindReset[code];
-						delete bindUpd[code];
-						delete repeatStore[code];
+		var _unbindObj = (obj, handler = _unbind) => {
+			for (var code = obj[_MASK]; code <= maxCode; code++) {
+				[repeatStore, bindReset].forEach(acc => {
+					if ((acc[code]) && (tmp = Array.from(acc[code]))) {
+						tmp.forEach(el => handler(el));
+						delete acc[code];
 					}
 				});
+
+				if ((tmp = bindUpd[code]) && (tmp = Object.values(tmp))) {
+					Object.values(tmp).forEach(itm => itm.forEach(el => handler(el)));
+					delete bindUpd[code];
+				}
 			}
 
 			return obj;
@@ -121,31 +128,26 @@ self.App = (() => {
 		var needStoredGetterFlg = false;
 		var skipProxySetFlg = false;
 
-		var buildData = (obj, code = 1, oldMap = Object.create(null)) => {
+		var buildData = (obj, code = 1) => {
 			var mask = code;
 			var props = Object.create(null);
+			var nextCode = code;
 
 			return new Proxy(obj, {
 				get: function(target, prop, receiver) {
-					if (prop === IS_PROXY) return true;
-					if (prop === MASK) return mask;
-					if (prop === MAP) return props;
+					if (prop === _IS_PROXY) return true;
+					if (prop === _MASK) return mask;
+					if (prop === _MAP) return props;
 
 					if (!skipProxyGetFlg) {
 						if (!props[prop]) {
-							if (oldMap[prop])
-								props[prop] = mask = oldMap[prop];
-							else {
-								mask <<= Object.keys(props).length;
-								props[prop] = mask;
-							}
-								
+							props[prop] = nextCode <<= Object.keys(props).length;
 							maxCode = Math.max(maxCode, mask);
 						}
 
-						if ((target[prop] instanceof Object) && !(target[prop][IS_PROXY])) {
+						if ((target[prop] instanceof Object) && !(target[prop][_IS_PROXY])) {
 							skipProxySetFlg = true;
-							receiver[prop] = buildData(target[prop], ((mask << 1) | 1), props);
+							receiver[prop] = buildData(target[prop], ((nextCode << 1) | 1));
 							skipProxySetFlg = false;
 						} else if (!obj2prox.has(target))
 							obj2prox.set(target, receiver);
@@ -160,12 +162,12 @@ self.App = (() => {
 					return Reflect.get(target, prop, receiver);
 				},
 
-				set: function(target, prop, val, receiver)  {
+				set: function(target, prop, val, receiver) {
 					var storebinds = null;
 					var storeRepeats = null;
 
-					if ((!skipProxySetFlg) && (val instanceof Object) && (!val[IS_PROXY]))
-						val = buildData(val, ((mask << 1) | 1), target[prop][MAP]);
+					if ((!skipProxySetFlg) && (val instanceof Object) && (!val[_IS_PROXY]))
+						val = buildData(val, ((mask << 1) | 1));
 
 					const result = Reflect.set(target, prop, val, receiver);
 
@@ -180,11 +182,17 @@ self.App = (() => {
 
 					return result;
 				},
+
+				deleteProperty: function(target, prop) {
+					if (tmp = target[prop]) {
+						if (tmp[_IS_PROXY]) _unbindObj(tmp, resetEl);
+					}
+				},
 			});
 		}
 
 		var extInterface = {
-			buildData: obj => rootObj = buildData(obj),
+			buildData: obj => buildData(obj),
 
 			bind: (elSel, hndl, args) => {
 				const callback = (el, cop) => cop.obj[cop.prop] = el[BINDING_PROPERTY];
@@ -225,7 +233,7 @@ self.App = (() => {
 				var group = Object.create(null);
 
 				if ((currentObjProp) && (xrBindCallbackOrFlag != null)) {
-					if (repeatStore[currentObjProp.obj[MASK]])
+					if (repeatStore[currentObjProp.obj[_MASK]])
 						updRepeat(elm, group);
 					else
 						addRepeat(extInterface.repeat.bind(null, elm, iterHandle, bindHandle, xrBindCallbackOrFlag), elm, group);
