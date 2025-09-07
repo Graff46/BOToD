cfa69de083125c3b9dd3aae48cff0288d1740f7c
Graff46 <macter46@yandex.ru>
2025-09-08 01:06:16 +0300

    Stress test

--- tests/test.js
+++ tests/test.js
@@ -1,17 +1,17 @@
 const obj = {
-    key: {
-        k1: {
-            l1: {m1: 1, m11: 2,}
-        }, 
-    },
+	key: {
+		k1: {
+			l1: {m1: 1, m11: 2,}
+		}, 
+	},
 
-    one: {
-        k2: {
-            l2: {m2: 22,}
-        }
-    },
+	one: {
+		k2: {
+			l2: {m2: 22,}
+		}
+	},
 
-    two: 5,
+	two: 5,
 };
 
 const myApp = App(obj, App.eventTypeInput);
@@ -20,14 +20,45 @@ const appData = myApp.buildData(obj);
 const y = appData.key;
 const yy = appData.one.k2;
 
-myApp.xrBind('.i3', x => x.value = y.k1.l1.m1, (el) => y.k1.l1.m1 = el.value);
-myApp.bind('.i1', x => y.k1.l1.m1);
+//myApp.xrBind('.i3', x => x.value = y.k1.l1.m1, (el) => y.k1.l1.m1 = el.value);
+//myApp.bind('.i1', x => y.k1.l1.m1);
 
-myApp.repeat('.i2', x => y.k1.l1, (el, k) => el.value = y.k1.l1[k], (el, k) => y.k1.l1[k] = el.value);
+//myApp.repeat('.i2', x => y.k1.l1, (el, k) => el.value = y.k1.l1[k], (el, k) => y.k1.l1[k] = el.value);
 //myApp.repeat('.i2', x => appData, (k) => appData[k] ? k: 0);
 var tt;
-setTimeout(() => { y.k1 = {l1:{ m1: 55 }}; }, 2000);
-setTimeout(() => { self.tt= 1; y.k1.l1.m111 = 3; /*App.bind('.i3', x => yy.l2.m2)*/;}, 3000);
-setTimeout(() => {y.k1.l1 = {m1: 4, m11: 5, m1111: 6 };}, 4000);
-setTimeout(() => delete y.k1, 5000);
-setTimeout(() => y.k1 = {l1: { m1: 66, m11: 77}}, 6000);
+// setTimeout(() => { y.k1 = {l1:{ m1: 55 }}; }, 2000);
+// setTimeout(() => { self.tt= 1; y.k1.l1.m111 = 3; /*App.bind('.i3', x => yy.l2.m2)*/;}, 3000);
+// setTimeout(() => {y.k1.l1 = {m1: 4, m11: 5, m1111: 6 };}, 4000);
+// setTimeout(() => delete y.k1, 5000);
+// setTimeout(() => y.k1 = {l1: { m1: 66, m11: 77}}, 6000);
+
+var max = 1000;
+async function render() {
+	let str = '';
+	for (let i = 1; i <= max; i++) {
+		str += `<div>
+			<hr>
+			<input type="text" class="i1${i}"> <br>
+			<input type="text" class="i2${i}"> <br>
+			<input type="text" class="i3${i}">
+		</div>`;
+	}
+
+	document.body.innerHTML += str;
+}
+
+async function runTest() {
+	console.time(1);
+	for (let i = 1; i <= max; i++) {
+		myApp.xrBind(`.i3${i}`, x => x.value = y.k1.l1.m1, (el) => y.k1.l1.m1 = el.value);
+		myApp.bind(`.i1${i}`, x => y.k1.l1.m1);
+		myApp.repeat(`.i2${i}`, x => y.k1.l1, (el, k) => el.value = y.k1.l1[k], (el, k) => y.k1.l1[k] = el.value);
+	}
+	console.timeEnd(1)
+
+	setTimeout(() => { y.k1 = {l1:{ m1: 55 }}; }, 2000);
+	setTimeout(() => { y.k1.l1.m111 = 3; myApp.unbind(`.i33`);}, 4000);
+	setTimeout(() => {y.k1.l1 = {m1: 4, m11: 5, m1111: 6 };}, 6000);
+	setTimeout(() => delete y.k1, 8000);
+	setTimeout(() => y.k1 = {l1: { m1: 66, m11: 77}}, 10_000);
+};
