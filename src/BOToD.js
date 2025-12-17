daae4afdfea3a7b1dec166f476204bcaf309fb22
Graff46 <macter46@yandex.ru>
2025-12-18 01:54:41 +0300

    interim commit

--- src/BOToD.js
+++ src/BOToD.js
@@ -399,10 +399,6 @@ self.App = (() => {
 		return extInterface;
 	};
 
-	Core.eventTypeInput = 0b1;
-	Core.textContentBinding = 0b10;
-	Core.$el = sel => getEl(sel);
-
 	Core.DOMBuilder = (docFragment, lastEl) => {
 		return new Proxy((...args) => {
 			if (!args[0].isEl) {
@@ -432,6 +428,39 @@ self.App = (() => {
 			set: () => {throw new SyntaxError("Don't set values!")},
 		});
 	}
+
+	Object.defineProperty(HTMLElement.prototype, 'bind', {
+		get: function() {
+			var el = this;
+			var props = {};
+			var obj = null;
+			var eventName = null;
+
+			return obj = new Proxy({}, {
+				set: (target, prop, value, receiver) => {
+					if (eventName) props[prop] = true;
+
+					(this.hndlrs = this.hndlrs || []).push(value);
+					el.addEventListener(eventName || prop, value, props);
+					props = {};
+				},
+				get: (target, prop, receiver) => {
+					if (!eventName)
+						eventName = prop;
+					else
+						props[prop] = true;
+					
+					return obj;
+				}
+			});
+		},
+		enumerable: false,
+		configurable: true  // важно: чтобы можно было определить
+	});
+
+	Core.eventTypeInput = 0b1;
+	Core.textContentBinding = 0b10;
+	Core.$el = sel => getEl(sel);
 
 	return Core;
 })();
