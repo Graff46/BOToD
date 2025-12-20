296ce226addca481d25eb7f61bee827022c1fa93
Graff46 <macter46@yandex.ru>
2025-12-20 15:25:26 +0300

    Separated futures

--- src/BOToD.js
+++ src/BOToD.js
@@ -429,38 +429,8 @@ self.App = (() => {
 		});
 	}
 
-	Object.defineProperty(HTMLElement.prototype, 'bind', {
-		get: function() {
-			var el = this;
-			var props = {};
-			var obj = null;
-			var eventName = null;
-
-			return obj = new Proxy({}, {
-				set: (target, prop, value, receiver) => {
-					if (eventName) props[prop] = true;
-
-					(this.hndlrs = this.hndlrs || []).push(value);
-					el.addEventListener(eventName || prop, value, props);
-					props = {};
-				},
-				get: (target, prop, receiver) => {
-					if (!eventName)
-						eventName = prop;
-					else
-						props[prop] = true;
-					
-					return obj;
-				}
-			});
-		},
-		enumerable: false,
-		configurable: true  // важно: чтобы можно было определить
-	});
-
 	Core.eventTypeInput = 0b1;
 	Core.textContentBinding = 0b10;
-	Core.$el = sel => getEl(sel);
 
 	return Core;
 })();
