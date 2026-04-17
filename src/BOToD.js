0ec34936e74dbaaffa88ffaafe96635582ddf5c9
sergey_werk <51144sergey_werk@users.no-reply.gitverse.ru>
2026-04-17 10:02:32 +0000

    Вроде работает BOToD

--- src/BOToD.js
+++ src/BOToD.js
@@ -81,9 +81,9 @@ self.App = (() => {
 
 		var addRepeat = (handler, el, group) => {
 			el2handlerRept.set(el, handler);
-			let last = null;
+			let last = '';
 
-			currentObjProp.sliceParents.forEach(prnt => (repeatStore[last = prnt] || (repeatStore[prnt] = new Set())).add(el));
+			currentObjProp.sliceParents.forEach(prnt => (repeatStore[last = prnt] || (repeatStore[last = prnt] = new Set())).add(el));
 
 			last = last.concat(currentObjProp.prop);
 			(repeatStore[last] || (repeatStore[last] = new Set())).add(el)
@@ -112,7 +112,6 @@ self.App = (() => {
 			const elm = getEl(el);
 
 			el2handlerBind.delete(elm);
-
 			elm[BINDING_PROPERTY] = null;
 
 			if (onlyBind) return;
@@ -173,6 +172,8 @@ self.App = (() => {
 				return acc;
 			}, []);
 
+			slice.unshift('');
+
 			return new Proxy(obj, {
 				get pKeys() { return matRow; },
 				sliceParents: slice,
@@ -190,11 +191,11 @@ self.App = (() => {
 
 						if ((target[prop] != null) ) {
 							if ((typeof(target[prop]) === 'object') && !(target[prop][_IS_PROXY])) {
-								//skipProxySetFlg = true;
+								skipProxySetFlg = true;
 								const selfParentProps = Array.from(parentProps);
 								selfParentProps.push(prop);
 								receiver[prop] = buildData(target[prop], deepLvl + 1, selfParentProps);
-								//skipProxySetFlg = false;
+								skipProxySetFlg = false;
 							}
 						}
 
@@ -233,7 +234,7 @@ self.App = (() => {
 
 
 					let storebinds = null, storeRepeats = null;
-					const key = this.sliceParents[this.sliceParents.length - 1];
+					const key = this.lastKey || '';
 
 					if (storeRepeats = repeatStore[key]) storeRepeats.forEach(el => (tmp = el2handlerRept.get(el)) && tmp(true));
 
@@ -469,7 +470,6 @@ self.App = (() => {
 				for (const k in props)
 					lastEl.setAttribute(k, props[k]);
 			}
-
 			if (args.length) args.shift()._getEl.forEach(el => lastEl.append(el.cloneNode(true)));
 
 			return Core.DOMBuilder(docFragment, lastEl);
